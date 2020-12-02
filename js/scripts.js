906347a745b5ced83d6cc729816586e2b79bc418
ComedyCenter101 <72399906+ComedyCenter101@users.noreply.github.com>
2020-12-02 16:57:19 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,11 @@
-// When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
-// Get the navbar
+
 var navbar = document.getElementById("navbar");
 
-// Get the offset position of the navbar
+
 var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
@@ -15,6 +13,7 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+			
 			
 var coll = document.getElementsByClassName("collapsible");
 var z;
@@ -63,3 +62,51 @@ function showText() {
     x.style.display = "none";
   }
 }
+
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("bpsfilterDiv");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "bpsshow");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "bpsshow");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var bpsbtnContainer = document.getElementById("bpsmyBtnContainer");
+var bpsbtns = bpsbtnContainer.getElementsByClassName("bpsbtn");
+for (var w = 0; w < bpsbtns.length; w++) {
+  bpsbtns[w].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace("active", "");
+    this.className += " active";
+  });
+}
+
